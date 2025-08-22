b403504ca6bb2b8c73a682cc8ce4ffc197985838
kensukemachii <kensukemachii@gmail.com>
2025-08-22 10:25:11 +0900

    feat: 変更内容を簡潔に

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://niemewebsite-o9l8.vercel.app',  // ← ここを確定値に
+  generateRobotsTxt: true,
+  outDir: 'public',
+};
