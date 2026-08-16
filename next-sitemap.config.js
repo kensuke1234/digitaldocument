d0387e2e11358386b7669f3ef99dc2a85dc70c80
kensukemachii <kensukemachii@gmail.com>
2026-08-16 17:12:22 +0900

    Fix production sitemap URL

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://niemewebsite-o9l8.vercel.app',  // ← ここを確定値に
+  siteUrl: 'https://digitaldocument.vercel.app',
   generateRobotsTxt: true,
   outDir: 'public',
 };
